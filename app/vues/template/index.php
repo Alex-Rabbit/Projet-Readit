164605fe23fc9ec000e3375d51a9dc835ec69586
Alexandre <massin.a.contact@gmail.com>
2020-05-25 17:25:34 +0200

    Amélioration de l'indentation

--- app/vues/template/index.php
+++ app/vues/template/index.php
@@ -12,9 +12,9 @@
 
 	<?php include_once '../app/vues/template/partials/_nav.php'; ?>
 
-    <?php include_once '../app/vues/template/partials/_hero.php'; ?>
+  <?php include_once '../app/vues/template/partials/_hero.php'; ?>
 
-   <?php include_once '../app/vues/template/partials/_main.php'; ?>
+  <?php include_once '../app/vues/template/partials/_main.php'; ?>
 
   <?php include_once '../app/vues/template/partials/_footer.php'; ?>
 
